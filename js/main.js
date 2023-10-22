dd49272d08d21c216ed6b70a140d5ad81ab64dfb
teastz <142979733+ZoraCode@users.noreply.github.com>
2023-10-23 02:20:19 +0900

    [Fix] Footer 반응형

--- js/main.js
+++ js/main.js
@@ -9,36 +9,35 @@ headerHeight = header.offsetHeight;
 let headerBottomOST = header.offsetTop;
 let headerSubmenuHeight = 0;
 
-// ul 리스트를 아이템에 담고
+
 headerSubmenu.forEach(item=>{
-// 만약 ul높이가 0(서브메뉴높이)보다 크면
+
   if(item.offsetHeight > headerSubmenuHeight){
-// 서브메뉴 높이는 ul 리스트의 높이
+
     headerSubmenuHeight = item.offsetHeight;
   }
 });
 
-// console.log(headerSubmenuHeight);
-// header총값 = 바텀메뉴OST + ul메뉴크기 + px
+
 let headerTotal = `${headerHeight + headerSubmenuHeight + 15}px`;
 
-//바텀메뉴에 마우스를 올리면 할일,일어날 일
+
 headerMenu.forEach(tem=>{
   tem.addEventListener('mouseover',()=>{
-    //css 헤더의 높이가 header총값으로 변경
+
       header.style.height = headerTotal;
   });
-    //마우스가 나가면 바텀메뉴OST로 돌아옴
+
   tem.addEventListener('mouseout',()=>{
       header.style.height = `${headerHeight}px`;
   });	
 });
-//스크롤이 생기면 할일
+
 window.addEventListener('scroll',()=>{
-  //만약 스크롤양이 헤더 높이보다 크면 header 클래스에 sticky 추가
+  
   if(window.scrollY > headerBottomOST){
       header.classList.add('sticky');
-  //아니라면header 클래스에 sticky 삭제
+  제
   }else{
       header.classList.remove('sticky');
   }
@@ -194,3 +193,19 @@ const sec3swiper = new Swiper('.sec3ab', {
   });
 
 
+  $(window).resize(function(){
+
+  })
+
+function footerDf(){
+  let screenWidth = $(window).width();
+  if(screenWidth <= 770){
+    $('footer').removeClass('d-flex');
+    $('footer > div').removeClass('d-flex');
+  }else{
+    $('footer').addClass('d-flex');
+    $('footer > div').addClass('d-flex');
+  }
+}
+
+footerDf();
