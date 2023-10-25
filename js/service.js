81628b21a5aded031e19a6e8a5e5954940eec30a
teastz <142979733+ZoraCode@users.noreply.github.com>
2023-10-25 15:16:05 +0900

    [Fix] 가이드 반응형

--- js/service.js
+++ js/service.js
@@ -46,63 +46,16 @@ window.addEventListener('scroll',()=>{
 
 
 // 탭메뉴
-let tabColor = $('.color'),
-tabList = $('.tab li'),
-tabs = $('.convenient li');
-
-tabList.on('click', function(e) {
-e.preventDefault();
-  colorspace = $(this).position().left,
-  colormove = $(this).width();
-
-  tabColor.css({
-  left: colorspace + 'px',
-  width: colormove + 'px'
-  });
-  //ul을 클릭하면 모든것에 active 지우고 클릭한 그것만 추가
-  tabList.children('a').removeClass('active');
-  $(this).children('a').addClass('active');
-
-  tabs.hide();
-  tabs.eq($(this).index()).show();
-
-  console.log(tabs);
-});
 
 
-function updateImage() {
-  let screenWidth = $(window).width();
-  if (screenWidth <= 705) {
 
-    $('.tab1 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    $('.tab2 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    $('.tab3 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    $('.tab4 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    $('.tab5 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    $('.tab6 .img_list img').attr('src', '../img/Ntab_img_a.png');
-    
-  } else {
-
-    $('.tab1 .img_list img').attr('src', '../img/tab_img_a.png');
-    $('.tab2 .img_list img').attr('src', '../img/tab_img_a.png');
-    $('.tab3 .img_list img').attr('src', '../img/tab_img_a.png');
-    $('.tab4 .img_list img').attr('src', '../img/tab_img_a.png');
-    $('.tab5 .img_list img').attr('src', '../img/tab_img_a.png');
-    $('.tab6 .img_list img').attr('src', '../img/tab_img_a.png');
-
-  }
-}
-updateImage();
-
-$(window).resize(function() {
-  updateImage();
-});
-
-/* Java Script로 한 것
+/* Java Script로 한 것 */
 let tabColor = document.querySelector('.color'),
     tabList = document.querySelectorAll('.tab li'),
     tabListA = document.querySelectorAll('.tab li a'),
     tabs = document.querySelectorAll('.convenient > li');
+
+
 
 
 tabList.forEach((tem,idx)=>{
@@ -119,36 +72,69 @@ tabList.forEach((tem,idx)=>{
       is.style.display = 'none';
     }
     tabs[idx].style.display = 'block';
+
+    
+    
   });
 });
 
 
 tabs.addEventListener('click',()=>{
   tabs.classList.add('avtive');
-
 });
-*/
 
-let list = $('.service .list');
-let listP = $('.service .list span p');
 
-list.each(function() {
-  let listEffect = $(this).find('div[class^="service_item"]');
+// function updateImage() {
+//   let screenWidth = $(window).width();
+//   if (screenWidth <= 705) {
+
+//     $('.tab1 .img_list img').attr('src', '../img/Ntab_img_a.png');
+//     $('.tab2 .img_list img').attr('src', '../img/Ntab_img_a.png');
+//     $('.tab3 .img_list img').attr('src', '../img/Ntab_img_a.png');
+//     $('.tab4 .img_list img').attr('src', '../img/Ntab_img_a.png');
+//     $('.tab5 .img_list img').attr('src', '../img/Ntab_img_a.png');
+//     $('.tab6 .img_list img').attr('src', '../img/Ntab_img_a.png');
+    
+//   } else {
+
+//     $('.tab1 .img_list img').attr('src', '../img/tab_img_a.png');
+//     $('.tab2 .img_list img').attr('src', '../img/tab_img_a.png');
+//     $('.tab3 .img_list img').attr('src', '../img/tab_img_a.png');
+//     $('.tab4 .img_list img').attr('src', '../img/tab_img_a.png');
+//     $('.tab5 .img_list img').attr('src', '../img/tab_img_a.png');
+//     $('.tab6 .img_list img').attr('src', '../img/tab_img_a.png');
+
+//   }
+// }
+// updateImage();
+
+// $(window).resize(function() {
+//   updateImage();
+// });
+
+
+// 
+
+// let list = $('.service .list');
+// let listP = $('.service .list span p');
+
+// list.each(function() {
+//   let listEffect = $(this).find('div[class^="service_item"]');
   
-  listEffect.mouseenter(function() {
-    $(this).closest('.list').addClass('active');
-    listP.addClass('active');
-  });
+//   listEffect.mouseenter(function() {
+//     $(this).closest('.list').addClass('active');
+//     listP.addClass('active');
+//   });
 
-  listEffect.mouseleave(function() {
-    $(this).closest('.list').removeClass('active');
-    listP.removeClass('active');
-  });
-});
+//   listEffect.mouseleave(function() {
+//     $(this).closest('.list').removeClass('active');
+//     listP.removeClass('active');
+//   });
+// });
 
 // javascript로 만든 것
 
-/* 
+ 
 
 let list = document.querySelector('.service .list'),
     listEffect = list.querySelectorAll('div[class^="service_item"]');
@@ -163,5 +149,3 @@ let list = document.querySelector('.service .list'),
 
   });
 });
-
-*/
