e8c3dac139cc71b4b507710688925c6568d64d78
teastz <142979733+ZoraCode@users.noreply.github.com>
2023-10-30 04:13:26 +0900

    [Fix] 팝업 탑버튼

--- js/main.js
+++ js/main.js
@@ -195,3 +195,98 @@ const sec3swiper = new Swiper('.sec3ab', {
   $(window).resize(function(){
 
   })
+
+
+// button
+let topBtn = document.querySelector('.top_button'),
+    topBtnOffset = document.querySelector('.sec2').offsetTop,
+    topScroll = 0;
+
+window.addEventListener('scroll',()=>{
+  topScroll = window.scrollY; 
+  if(topScroll > topBtnOffset - 60){
+    topBtn.classList.add('active');
+  } else{
+    topBtn.classList.remove('active');
+  }
+});
+
+topBtn.addEventListener('click',()=>{
+  window.scrollTo({
+    left:0,
+    top:0,
+    behavior:'smooth'
+  });
+});
+
+
+
+
+
+
+// popup
+
+
+let popup = document.querySelector('.popup');
+let popup_closeBtn = popup.querySelector('#close');
+let popup_input = popup.querySelector('#daycheck');
+
+
+popup.querySelector('.ppt').addEventListener('click',()=>{
+  window.open('https://www.figma.com/file/XFGUZvuVHziJ0f2nk61xgr/Untitled?type=design&node-id=0-1&mode=design', '_blank');
+});
+
+popup.querySelector('.git').addEventListener('click',()=>{
+  window.open('https://github.com/ZoraCode/CaribbeanBay', '_blank');
+});
+
+// setCookie('starbucks','popup',1)
+//쿠키 만들기
+//name - 쿠키이름 / value - 쿠키값 / day - 쿠키기한
+function setCookie(name, value, day){
+  //day - 오늘날짜부터 정해진 기한까지 설정
+  let date = new Date();
+  date.setDate(date.getDate() + day);
+  
+  document.cookie = `${name}=${value};expires= ${date.toUTCString()}`;
+}//setCookie func
+
+
+//쿠키 있는지 확인해서 popup 보일지 결정
+function cookieCheck(name){
+  //존재하는 쿠키들을 배열로 만들기
+  let cookieArr = document.cookie.split(';');
+  let visited = false;
+
+  //쿠키 배열 중
+  for(let cookie of cookieArr){
+    //지정한 쿠키가 있으면
+    if(cookie.search(name) > -1){
+      //visited를 true로 변경하기(+ dialog 숨기기)
+      visited = true;
+      break;
+    }
+  }
+
+  //만약 visited의 값이 false면 dialog가 보인다
+  if(!visited){
+    popup.setAttribute('open','');
+  }
+
+}//cookieCheck func
+cookieCheck('starbucks');
+
+
+//close클릭하면
+popup_closeBtn.addEventListener('click',()=>{
+  //'오늘안보기'의 체크 유무에 상관없이 팝업을 닫는다
+  popup.removeAttribute('open');
+
+  //만약 '오늘안보기'에 체크 되어있다면, 쿠키만들고(기한 1일) 아니면 제거
+  if(popup_input.checked){
+    setCookie('starbucks','popup',1)
+  } else{
+    setCookie('starbucks','popup',-1)
+  }
+});//closeBtn click
+/* popup */
